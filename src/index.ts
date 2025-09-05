9e07ade23c955d5395309e1200d4312cafe8e287
Carson Gao <156142273+CarsonGao00140@users.noreply.github.com>
2025-09-05 17:54:17 -0400

    read config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-setInterval(() => console.log('Hello, World!'), 1000);
+import koa from 'koa';
+
+import config from './utils/config';
+
+const app = new koa();
+
+app.listen(config.port, () =>
+    console.log(`Server listening on port ${config.port}`)
+);
---
+++ src/utils/config.ts
@@ -0,0 +1,24 @@
+import fs from 'node:fs/promises';
+import { z } from 'zod';
+import yaml from 'yaml';
+
+const argsSchema = z.tuple([
+    z.literal('-c'),
+    z.string().regex(/\.yaml$/),
+])
+
+const configSchema = z.object({
+    port: z.number().int().min(1024).max(65535),
+    screen_width: z.number().int().positive(),
+    screen_height: z.number().int().positive(),
+}).strict();
+
+const args = argsSchema.parse(process.argv.slice(2));
+const file = await fs.readFile(args[1], 'utf-8');
+const yamlConfig = configSchema.parse(yaml.parse(file));
+
+export default {
+    port: yamlConfig.port,
+    screenWidth: yamlConfig.screen_width,
+    screenHeight: yamlConfig.screen_height,
+}
